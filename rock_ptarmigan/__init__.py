e606345b75a2c4eca4e7d5e5f3ca50797adae7ba
T・K <karuma.reason@gmail.com>
2025-11-24 13:26:23 +0900

    Update.

--- rock_ptarmigan/__init__.py
+++ rock_ptarmigan/__init__.py
@@ -1,9 +1,13 @@
 from importlib.metadata import version
 
-from .main import compare_after
+from .main import compare_all
+from .main import compare_train
+from .main import compare_validation
 
 # function call
-__all__ = ['compare_after']
+__all__ = ['compare_all']
+__all__ = ['compare_train']
+__all__ = ['compare_validation']
 
 # version
 __version__ = version(__package__)
